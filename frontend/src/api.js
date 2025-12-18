b793a86ff62a22df56c3255b90e56953644d6127
Sckxzin <kaykylz069@gmail.com>
2025-12-18 13:34:51 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://motonow-gestao-production.up.railway.app"
+  baseURL: "https://believable-harmony-production.up.railway.app",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
