b921aae8fd022c9b80ceef89583e7ea82e04980e
Sckxzin <kaykylz069@gmail.com>
2026-01-13 08:01:16 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -456,21 +456,9 @@ if (brinde === true) {
     [capacete.id]
   );
 }
-const mensagem = `
-🛵 VENDA DE MOTO
-Cliente: ${nome_cliente}
-Modelo: ${moto.modelo}
-Valor: R$ ${valor}
-Pagamento: ${forma_pagamento}
-Filial: ${filial_venda}
-`;
-
-const linkWhatsApp = gerarLinkWhatsApp(telefone, mensagem);
 
     await client.query("COMMIT");
-    res.json({ message: "Moto vendida com sucesso",
-whatsapp: linkWhatsApp
- });
+    res.json({ message: "Moto vendida com sucesso" });
 
   } catch (err) {
     await client.query("ROLLBACK");
@@ -480,11 +468,6 @@ whatsapp: linkWhatsApp
     client.release();
   }
 });
-function gerarLinkWhatsApp(telefone, mensagem) {
-  const numero = telefone.replace(/\D/g, "");
-  const texto = encodeURIComponent(mensagem);
-  return `https://wa.me/5581985206212?text=${texto}`;
-}
 
 /* ================= SERVER ================= */
 const PORT = process.env.PORT || 8080;
