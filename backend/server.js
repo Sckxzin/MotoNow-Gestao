ee92727f2647a0ab61fc9a6dca422163064ac699
Sckxzin <kaykylz069@gmail.com>
2026-01-17 10:19:19 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -405,6 +405,7 @@ app.get("/vendas", async (req, res) => {
   }
 });
 
+
 /* ================= HISTÓRICO VENDAS MOTOS ================= */
 app.get("/vendas-motos", async (req, res) => {
   try {
@@ -426,19 +427,7 @@ app.get("/vendas-motos", async (req, res) => {
         gasolina,
         como_chegou,
         santander,
-        created_at,
-
-        -- 🔒 ESCONDE EMPRESA E CNPJ SE FOR SANTANDER
-        CASE
-          WHEN santander = true THEN NULL
-          ELSE nome_empresa
-        END AS nome_empresa,
-
-        CASE
-          WHEN santander = true THEN NULL
-          ELSE cnpj_empresa
-        END AS cnpj_empresa
-
+        created_at
       FROM vendas_motos
       ORDER BY created_at DESC
     `);
