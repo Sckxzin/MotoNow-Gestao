dd3d1cd5eff95ccc0efe9276ba4f4ed847422ecd
Sckxzin <kaykylz069@gmail.com>
2026-01-24 10:24:21 -0300

    Change displayed Chassi to Chassi Moto

--- frontend/src/pages/NotaFiscal.jsx
+++ frontend/src/pages/NotaFiscal.jsx
@@ -47,7 +47,7 @@ export default function NotaFiscal() {
         <p><strong>Nome:</strong> {venda.cliente_nome}</p>
         <p><strong>CPF:</strong> {venda.cliente_cpf || "-"}</p>
         <p><strong>Telefone:</strong> {venda.cliente_telefone || "-"}</p>
-        <p><strong>Chassi:</strong> {venda.chassi || "-"}</p>
+        <p><strong>Chassi:</strong> {venda.chassi_moto || "-"}</p>
       </div>
 
       {/* ITENS */}
