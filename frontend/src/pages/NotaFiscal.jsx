2927343e654e1c499546cffb71741f9ffdbcdda9
Sckxzin <kaykylz069@gmail.com>
2026-02-19 11:35:12 -0300

    Rename SAO JOSE to SAOJOSE in NotaFiscal.jsx

--- frontend/src/pages/NotaFiscal.jsx
+++ frontend/src/pages/NotaFiscal.jsx
@@ -37,7 +37,7 @@ export default function NotaFiscal() {
       telefone: "(81) 99245-9495",
       cnpj: "58.021.497/0001-04",
     },
-    SAO JOSE: {
+    SAOJOSE: {
      nome: "LITORAL MOTO CENTER SHINERAY LTDA",
      endereço: "PE-060, 60",
      cidade: "São José da Coroa Grande",
