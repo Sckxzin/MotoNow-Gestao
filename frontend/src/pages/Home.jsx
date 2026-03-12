02082269728482844e44d846931060b608a65e28
Sckxzin <kaykylz069@gmail.com>
2026-03-12 10:56:04 -0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,6 +44,7 @@ const REPASSE_SANTANDER_POR_MODELO = {
   [normModeloKey("NEW JET 125")]: 9500,
   [normModeloKey("URBAN 150 EFI")]:18100,
   [normModeloKey("IRON 250")]:19600,
+  [normModeloKey("ATV 125 EFI")]:15000,
 };
 
 function isRepasseObrigatorio(filial) {
